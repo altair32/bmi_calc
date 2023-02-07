0043bc6d3642d57a5ccafc7992f4526ae171bd9c
altair32 <PARAM31.10000@gmail.com>
2023-02-07 18:27:21 +0530

    1st Commit

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(BMICalc());
+}
+class BMICalc extends StatelessWidget {
+  const BMICalc({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: inputpage(),
+    );
+  }
+}
+class inputpage extends StatefulWidget {
+  const inputpage({Key? key}) : super(key: key);
+
+  @override
+  State<inputpage> createState() => _inputpageState();
+}
+
+class _inputpageState extends State<inputpage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("BMI Calculator"),
+      ),
+      body: Center(
+        child: Text("Body"),
+      ),
+      floatingActionButton: ,
+    );
+  }
+}
+
+
+
